9f7965f424ae1e6cd47634f179f80729eb507535
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-11-01 16:46:33 -0400

    integrates react router

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,27 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
-import App from './App';
+import { WrappedApp, App } from './App';
 
 describe('App', () => {
   it('renders the app and displays it', () => {
-    // ARRANGE
-    render(<App />);
+    render(<WrappedApp />);
 
-    // ACT
-
-    // ASSERT
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
       'Hello World!'
+    );
+  });
+
+  it('Renders not found if path is invalid', () => {
+    render(
+      <MemoryRouter initialEntries={['/foo']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'Not Found'
     );
   });
 });

ac96a671cd4425b6bcda731c24e1e591911cbc2a
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-11-01 15:41:48 -0400

    fixes import for jest-dom matchers

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom/vitest';
-import matchers from '@testing-library/jest-dom/matchers';
+import * as matchers from '@testing-library/jest-dom/matchers';
 import { expect } from 'vitest';
 
 expect.extend(matchers);

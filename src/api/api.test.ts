8ad312b3974ce3d127e6830e2d6f4ec89d931e14
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-11-13 10:26:22 -0500

    improves test coverage

--- src/api/api.test.ts
+++ src/api/api.test.ts
@@ -1,6 +1,52 @@
 import { describe, it, expect } from 'vitest';
-import api from './api';
+import { z } from 'zod';
+import api, { HTTPMethod } from './api';
+import {
+  GetEncountersRequest,
+  GetEncountersResponse,
+} from './encounterRequests';
+import encounters from '../mocks/encounters';
+
+const validRequest = GetEncountersRequest;
+const validResponse = GetEncountersResponse;
+const invalidResponse = z.void();
+
+const validEncounterRequest = api<
+  z.infer<typeof validRequest>,
+  z.infer<typeof validResponse>
+>({
+  method: HTTPMethod.GET,
+  path: 'https://dummy/encounters',
+  requestSchema: validRequest,
+  responseSchema: validResponse,
+});
+
+const invalidEncounterRequest = api<
+  z.infer<typeof validRequest>,
+  z.infer<typeof invalidResponse>
+>({
+  method: HTTPMethod.GET,
+  path: 'https://dummy/encounters',
+  requestSchema: validRequest,
+  responseSchema: invalidResponse,
+});
 
 describe('api', () => {
-  it('should return a function', () => { });
+  it('should return a Promise', () => {
+    const result = validEncounterRequest();
+
+    expect(result).toHaveProperty('then');
+  });
+
+  it('should resolve to the correct data if the response is valid', async () => {
+    const result = await validEncounterRequest();
+
+    expect(result).toEqual(encounters);
+  });
+
+  it('should return an empty array if the response is not valid', async () => {
+    const result = await invalidEncounterRequest();
+
+    expect(result).toEqual([]);
+  });
 });

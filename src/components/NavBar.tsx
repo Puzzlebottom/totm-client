49fb81df01398cfd78cfb65d277d8e8e97a67dd7
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-11-18 13:19:17 -0500

    styles the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,34 +1,40 @@
 import { Link } from 'react-router-dom';
 
+import '../styles/navBar.css';
+
 interface Props {
   links: string[];
 }
 
 export default function NavBar({ links }: Props): React.ReactNode {
   return (
-    <nav>
-      <h2>
-        <Link to="/">TOTM</Link>
-      </h2>
-      <ul>
+    <nav className="navbar" aria-label="main">
+      <Link to="/" className="navbar-brand">
+        TOTM
+      </Link>
+      <span>
         {links.map((link) => (
-          <li key={link}>
-            <Link to={link}>{link}</Link>
-          </li>
+          <Link key={link} to={link} className="nav-link">
+            {link}
+          </Link>
         ))}
-      </ul>
-      <ul>
-        <li>
-          <button type="button" aria-label="login" className="outline">
-            Login
-          </button>
-        </li>
-        <li>
-          <button type="button" aria-label="register" className="outline">
-            Register
-          </button>
-        </li>
-      </ul>
+      </span>
+      <span>
+        <button
+          type="button"
+          aria-label="login"
+          className="button-primary-outline"
+        >
+          Login
+        </button>
+        <button
+          type="button"
+          aria-label="register"
+          className="button-primary-outline"
+        >
+          Register
+        </button>
+      </span>
     </nav>
   );
 }

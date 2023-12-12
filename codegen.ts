80dcd4582ffb4026afc97ca5a2c73c65f7a6cf08
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-12-12 10:44:31 -0500

    adds and configures codegen to generate graphql schema on server startup

---
+++ codegen.ts
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: 'http://localhost:3000/graphql',
+  documents: ['src/**/*.tsx'],
+  generates: {
+    './src/__generated__/': {
+      preset: 'client',
+      presetConfig: {
+        gqlTagName: 'gql',
+      },
+    },
+  },
+  ignoreNoDocuments: true,
+};
+
+export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import codegen from 'vite-plugin-graphql-codegen';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), codegen()],
   test: {
     globals: true,
     environment: 'jsdom',
